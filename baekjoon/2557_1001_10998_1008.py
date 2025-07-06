36b02eb0e31519527136fd7ed114d2346f95ee15
totocm00 <typesapk@naver.com>
2025-07-07 00:00:20 +0900

    solve: 백준 네문제

---
+++ baekjoon/2557_1001_10998_1008.py
@@ -0,0 +1,14 @@
+# 2557 Hello World!를 출력하시오.
+print("Hello World!")
+
+# 1001 A - B 출력
+a, b = map(int, input().split())
+print(a-b)
+
+# 10998 A x B
+a, b = map(int, input().split())
+print(a*b)
+
+# 1008 A / B
+a, b = map(float, input().split())
+print(a/b)
