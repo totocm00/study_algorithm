e2293c0a041a437578d1e8961a8956bee1a5804b
totocm00 <typesapk@naver.com>
2025-07-18 23:56:22 +0900

    refactor: 0718 _ str.swapcase()

--- programmers/20250718.py
+++ programmers/20250718.py
@@ -1,8 +1,2 @@
 input_str = input()
-result = ""
-for c in input_str:
-    if c.isupper():
-        result += c.lower()
-    else:
-        result += c.upper()
-print(result)
+print(input_str.swapcase())
