c94b688cdfda5b3a472d5737b84a7a9ca7f64624
totocm00 <typesapk@naver.com>
2025-07-19 23:58:40 +0900

    refactor: 10951 - EOF 변경 풀이 및 노션에 sys.stdin: 정리

--- baekjoon/repeatingSentence/10951.py
+++ baekjoon/repeatingSentence/10951.py
@@ -1,10 +1,8 @@
 import sys
-input = sys.stdin.readline
-write = sys.stdout.write
 
-while 1:
-    try:
-        A, B = map(int, input().split())
-        write(f"{A+B}\n")
-    except ValueError:
-        break
+# EOF = End Of File, 입력이 끝났다는 신호
+# 더이상 입력이 없으면 자동으로 EOF 발생
+
+for line in sys.stdin:
+    A, B = map(int, line.split())
+    print(A + B)
